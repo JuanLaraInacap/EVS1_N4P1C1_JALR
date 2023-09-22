756edc92c8b33c5ec4116f65510ba7327d25b1ff
Juan <juan.lara05@inacapmail.cl>
2023-09-22 11:25:05 -0300

    Vista 3 Rama 2 Terminada

---
+++ jalr_app2/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def vistaTres(request):
+    html="""
+    <h1>Mejor Videojuego del Mundo</h1>
+    <h2>Vista 3</h2>
+    <h3>Rama 2</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dqls4wh_zg4?si=K2FV2Q4Jd8sH9XQ-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    """
+    return HttpResponse(html)
